87d0e044edd155c00f1ddab5c8fb8825de454d0c
tonwayer <tonwayer@gmail.com>
2025-12-29 04:43:29 -0500

    fixed: tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,12 +1,5 @@
-export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 };
